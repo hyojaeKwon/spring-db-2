06ff9d80517ac4fd55eb2fd950a91c488da8aea0
hyojaeKwon <khj010909@gm.gist.ac.kr>
2024-08-04 17:46:21 +0900

    test transaction

--- src/test/java/hello/itemservice/domain/ItemRepositoryTest.java
+++ src/test/java/hello/itemservice/domain/ItemRepositoryTest.java
@@ -5,32 +5,48 @@ import hello.itemservice.repository.ItemSearchCond;
 import hello.itemservice.repository.ItemUpdateDto;
 import hello.itemservice.repository.memory.MemoryItemRepository;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.internal.util.Platform;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.AutoConfigurationPackage;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@Transactional
 @SpringBootTest
 class ItemRepositoryTest {
 
     @Autowired
     ItemRepository itemRepository;
 
-    @AfterEach
-    void afterEach() {
-        //MemoryItemRepository 의 경우 제한적으로 사용
-        if (itemRepository instanceof MemoryItemRepository) {
-            ((MemoryItemRepository) itemRepository).clearStore();
-        }
-    }
+//    @Autowired
+//    PlatformTransactionManager transactionManager;
+//    TransactionStatus status;
+//
+//    @BeforeEach
+//    void beforeEach() {
+//        // transaction 시작
+//        status = transactionManager.getTransaction(new DefaultTransactionDefinition());
+//    }
+//
+//    @AfterEach
+//    void afterEach() {
+//        // transaction rollback
+//        transactionManager.rollback(status);
+//    }
 
     @Test
     void save() {
         //given
-        Item item = new Item("itemA", 10000, 10);
+        Item item = new Item("itemTest", 10000, 10);
 
         //when
         Item savedItem = itemRepository.save(item);
@@ -38,7 +54,9 @@ class ItemRepositoryTest {
         //then
         Item findItem = itemRepository.findById(item.getId()).get();
         assertThat(findItem).isEqualTo(savedItem);
+//        assertThat(findItem.getId()).isEqualTo(savedItem.getId());
     }
+
 
     @Test
     void updateItem() {
@@ -52,10 +70,10 @@ class ItemRepositoryTest {
         itemRepository.update(itemId, updateParam);
 
         //then
-        Item findItem = itemRepository.findById(itemId).get();
-        assertThat(findItem.getItemName()).isEqualTo(updateParam.getItemName());
-        assertThat(findItem.getPrice()).isEqualTo(updateParam.getPrice());
-        assertThat(findItem.getQuantity()).isEqualTo(updateParam.getQuantity());
+//        Item findItem = itemRepository.findById(itemId).get();
+//        assertThat(findItem.getItemName()).isEqualTo(updateParam.getItemName());
+//        assertThat(findItem.getPrice()).isEqualTo(updateParam.getPrice());
+//        assertThat(findItem.getQuantity()).isEqualTo(updateParam.getQuantity());
     }
 
     @Test
@@ -69,7 +87,7 @@ class ItemRepositoryTest {
         itemRepository.save(item2);
         itemRepository.save(item3);
 
-        //둘 다 없음 검증
+        //둘  다 없음 검증
         test(null, null, item1, item2, item3);
         test("", null, item1, item2, item3);
 
